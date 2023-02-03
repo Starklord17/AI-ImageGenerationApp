798c4b1684f2309c98415cd798b9366d1348a98d
MaxSM <maxi.sm117@gmail.com>
2023-02-03 18:17:51 -0300

    :zap: dalleRoute complete

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,4 +16,25 @@ router.route('/').get((req, res) => {
   res.send('Hello from DALL-E!');
 })
 
+router.route('/').post(async (req, res) => {
+  try {
+    const {prompt} = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message)
+  }
+})
+
 export default router;
